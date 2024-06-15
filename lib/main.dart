d51222a31c2bb57c697dd14f0efba1ec0a41d600
Walid Tolba <wrt0624@gmail.com>
2024-06-15 10:09:33 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,43 +31,27 @@ class MyApp extends StatelessWidget {
                   'Walid Tolba',
                   style: TextStyle(fontSize: 40, color: Colors.white, fontFamily: 'Pacifico'),
                 ),
-                Text('Mobile Developer'.toUpperCase(), style: TextStyle(fontSize: 14, color: Colors.teal.shade100, fontFamily: 'SourceSans3', letterSpacing: 2.5, fontWeight: FontWeight.bold),),
+                Text('Mobile Developer'.toUpperCase(), style: TextStyle(fontSize: 18, color: Colors.teal.shade100, fontFamily: 'SourceSans3', letterSpacing: 2.5, fontWeight: FontWeight.bold),),
                 Divider(
+                  color: Colors.teal.shade100,
                   thickness: 1,
                   indent: 100,
                   endIndent: 100,
                 ),
-                Container(
+                Card(
                   margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.call, color: Colors.teal),SizedBox(width: 20,),
-                      Text('+214-234-535-345', style: TextStyle(color: Colors.teal, fontFamily: 'SourceSans3', fontSize: 20),),
-                    ],
-                  ),
-                ),
-                Container(
+                  
+                  child: ListTile(leading: Icon(Icons.call, color: Colors.teal), title: Text('+214-234-535-345', style: TextStyle(color: Colors.teal, fontFamily: 'SourceSans3', fontSize: 20),))
+                ),Card(
                   margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.email, color: Colors.teal),SizedBox(width: 20,),
-                      Text('wrt@gmail.com', style: TextStyle(color: Colors.teal, fontFamily: 'SourceSans3', fontSize: 20),),
-                    ],
-                  ),
+                  
+                  child: ListTile(leading: Icon(Icons.email, color: Colors.teal), title: Text('wrt@gmail.com', style: TextStyle(color: Colors.teal, fontFamily: 'SourceSans3', fontSize: 20),))
                 ),
+                
               ],
             ),
           )),
     );
   }
 }
+
